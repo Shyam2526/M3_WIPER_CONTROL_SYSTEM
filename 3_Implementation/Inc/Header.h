e3e66788b0315a1eaf1996f36d452a930960864d
shyamkumar <shyamakumar4516@gmail.com>
2022-05-14 17:52:00 +0530

    psuh

---
+++ 3_Implementation/Inc/Header.h
@@ -0,0 +1,10 @@
+GPIO_Handle_t Gpio;
+void delay(uint32_t sec);
+void Led_Pin_Init(void);
+void Push_Button_Init(void);
+int wiper_led_On_Low(void);
+int wiper_led_On_Mid(void);
+int wiper_led_On_High(void);
+void car_starts(void);
+int wiper_led_Off(void);
+void car_Off(void);
--- 3_Implementation/Src/main.c
+++ 3_Implementation/Src/main.c
@@ -1,7 +1,12 @@
 #include "MyStm32f407xx.h"
 #include "Header.h"
 #include "MyStm32f407xx_gpio_driver.h"
-uint8_t switching=0;
+
+#define Hz	10000000
+
+int dum, dum1, dum2;
+uint8_t key=1;
+
 int main(void)
 {
 	Gpio.pGPIOx = GPIOD;
@@ -17,20 +22,23 @@ int main(void)
 	{
 		Push_Button_Init();
 		//GPIO_WriteToOutputPin(GPIOA, GPIO_PIN_NO_0, DISABLE);
-		if((GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)))
+		if((GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))==ENABLE)
 		{
 		if(key==1)
 		{
+			delay(3);
+			delay(3);
+			delay(3);
 			car_starts();
-			if((GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))){
 				key++;
-			}
+
 		}
 		else if(key==2)
 		{
 			while(key==2){
-			wiper_led_On_Low();
-			if((GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))){
+			dum=wiper_led_On_Low();
+			if(dum==ENABLE)
+			{
 				key++;
 				break;
 			}
@@ -39,34 +47,41 @@ int main(void)
 		else if(key==3)
 		{
 			while(key==3){
-				wiper_led_On_Mid();
-			if((GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))){
-				key++;
-				break;
-			}
+				dum1=wiper_led_On_Mid();
+				if(dum1==ENABLE)
+				{
+					key++;
+					break;
+				}
 		}
 		}
 		else if(key == 4){
 			while(key==4){
-				wiper_led_On_High();
-			if((GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))){
-				key++;
-				break;
-			}
+				dum2=wiper_led_On_High();
+				if(dum2==ENABLE)
+				{
+					key++;
+					break;
+				}
 			}
 		}
 		else if(key==5){
+			delay(3);
+			delay(3);
+			delay(3);
 			wiper_led_Off();
+			key++;
 		}
-	}
 		else
-			car_off();
+			car_Off();
+	}
+
 	}
 }
-void delay(uint32_t sec)
- {
-	 for (uint32_t i = 0; i < sec*900000; i++);
- }
+
+void delay(uint32_t sec){
+	for(uint32_t i= 0; i < sec*Hz; i++);
+}
 
  void Led_Pin_Init(void)
  {
@@ -87,39 +102,30 @@ void delay(uint32_t sec)
  {
 	 GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
 }
-void car_off(void){
+void car_Off(void){
+	key=0;
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, DISABLE);
 }
- void wiper_led_On_Low(void){
+int wiper_led_On_Low(void){
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
-	delay(150);
+	delay(9);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, DISABLE);
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
-	delay(150);
+	delay(9);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
-	delay(150);
+	delay(9);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, DISABLE);
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
-	delay(150);
+	delay(9);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
+	if((GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))==ENABLE){
+		//break;
+		return 1 ;
+	}
+	return 0;
  }
- void wiper_led_On_Mid(void)
- {
-	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
-	delay(50);
-	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, DISABLE);
-	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
-	delay(50);
-	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
-	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
-	delay(50);
-	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, DISABLE);
-	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
-	delay(50);
-	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
- }
- void wiper_led_On_High(void)
+int wiper_led_On_Mid(void)
  {
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
 	delay(5);
@@ -133,16 +139,42 @@ void car_off(void){
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
 	delay(5);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
+	if((GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))==ENABLE){
+		//break;
+		return 1;
+	}
+	return 0;
+ }
+ int wiper_led_On_High(void)
+ {
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+	delay(2);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, DISABLE);
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	delay(2);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+	delay(2);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, DISABLE);
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	delay(2);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
+	if((GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))==ENABLE){
+		//break;
+		return 1;
+	}
+	return 0;
 }
-void wiper_led_Off(void)
+int wiper_led_Off(void)
 {
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
-	delay(50);
+	delay(8);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, DISABLE);
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
-	delay(50);
+	delay(8);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
-	delay(50);
+	delay(8);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, DISABLE);
+	return 0;
 }
